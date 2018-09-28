7af9ea19fa16bf43897379149f34f74d6f7c4a86
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-09-28 18:43:33 -0300

    Add Data routes and nested routes: check, list, add

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import Home from './routes/Home.html';
 import Request from './routes/Request.html';
+import Data from './routes/Data/Home.html';
+import DataTemplate from './routes/Data/Data.html';
+import DataList from './routes/Data/List.html';
+import DataSave from './routes/Data/Save.html';
+import DataCheck from './routes/Data/Check.html';
 
 export default {
   '/': Home,
   '/request': Request,
+  '/data': Data,
+  '/data/*': DataTemplate,
+  '/data/save': DataSave,
+  '/data/list': DataList,
+  '/data/check/:user': DataCheck,
 };
