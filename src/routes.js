553308378a93ae1a6e80ecc6c530b71beebae1f6
Christian Kaisermann <christian@kaisermann.me>
2018-10-01 16:10:43 -0300

    Pair programming refactor ~

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import Home from './routes/Home.html';
-import Request from './routes/Request.html';
-import Data from './routes/Data/Home.html';
-import DataTemplate from './routes/Data/Data.html';
-import DataList from './routes/Data/List.html';
-import DataSave from './routes/Data/Save.html';
-import DataCheck from './routes/Data/Check.html';
+import Request from './routes/1-request/Request.html';
+import Data from './routes/2-dados/Home.html';
+import DataList from './routes/2-dados/List.html';
+import DataAdd from './routes/2-dados/Add.html';
+import DataUser from './routes/2-dados/User.html';
 
 export default {
   '/': Home,
-  '/request': Request,
-  '/data': Data,
-  '/data/*': DataTemplate,
-  '/data/save': DataSave,
-  '/data/list': DataList,
-  '/data/check': DataCheck,
+  '/1-request': Request,
+  '/2-dados': Data,
+  '/2-dados/add': DataAdd,
+  '/2-dados/list': DataList,
+  '/2-dados/user': DataUser,
 };
