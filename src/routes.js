9f5b75fe7910035d9c393bf33ed66ef8384094e1
Vitor Paladini <vitor@paladini.in>
2018-10-17 16:38:21 +0000

    Add post request poc

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import Home from './routes/Home.html';
-import Request from './routes/1-request/Request.html';
-import Data from './routes/2-dados/Home.html';
-import DataList from './routes/2-dados/List.html';
-import DataAdd from './routes/2-dados/Add.html';
-import DataUser from './routes/2-dados/User.html';
+import RequestGet from './routes/1-request-get/RequestGet.html';
+import RequestPost from './routes/2-request-post/RequestPost.html';
+import Data from './routes/3-dados/Home.html';
+import DataList from './routes/3-dados/List.html';
+import DataAdd from './routes/3-dados/Add.html';
+import DataUser from './routes/3-dados/User.html';
 
 export default {
   '/': Home,
-  '/1-request': Request,
-  '/2-dados': Data,
-  '/2-dados/add': DataAdd,
-  '/2-dados/list': DataList,
-  '/2-dados/user': DataUser,
+  '/1-request-get': RequestGet,
+  '/2-request-post': RequestPost,
+  '/3-dados': Data,
+  '/3-dados/add': DataAdd,
+  '/3-dados/list': DataList,
+  '/3-dados/user': DataUser,
 };
